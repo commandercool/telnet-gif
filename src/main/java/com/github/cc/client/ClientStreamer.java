3cc7f6c76b3ce94e7a85829820fbf3aab3a5fcab
commandercool <erohin_a_v@mail.ru>
2021-04-12 19:32:13 +0300

    Initial commit

---
+++ src/main/java/com/github/cc/client/ClientStreamer.java
@@ -0,0 +1,48 @@
+package com.github.cc.client;
+
+import static com.github.cc.image.Animation.getFrames;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import com.github.cc.image.Animation;
+
+public class ClientStreamer implements Runnable {
+
+    private final Socket client;
+
+    public ClientStreamer(Socket client) {
+        this.client = client;
+    }
+
+    public void run() {
+        try {
+            OutputStream outputStream = client.getOutputStream();
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            printWriter.println("\u001B[2J");
+            int maxCycles = 4;
+            while (!client.isClosed() && maxCycles-- > 0) {
+                for (Animation.Frame frame : getFrames()) {
+                    printWriter.print(frame.getAsciiImage());
+                    printWriter.flush();
+                    try {
+                        Thread.sleep(frame.getDelayTime() * 10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    printWriter.println("\u001B[1H");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                client.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/github/cc/image/Animation.java
@@ -0,0 +1,83 @@
+package com.github.cc.image;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import com.github.cc.decoder.GifDecoder;
+
+public class Animation {
+
+    private static final char[] PIXEL = {' ', '.', '*', ':', 'o', '&', '8', '#', '@'};
+    private static final int STEP = 255 / 9;
+
+    private static Frame[] frames;
+
+    public static void init() {
+        try {
+            final GifDecoder.GifImage gif =
+                    GifDecoder.read(Animation.class.getClassLoader().getResourceAsStream("card-cropped.gif"));
+            final int frameCount = gif.getFrameCount();
+            frames = new Frame[frameCount];
+            for (int i = 0; i < frameCount; i++) {
+                final BufferedImage img = gif.getFrame(i);
+                frames[i] = new Frame(convertToASCII(prepare(img)), gif.getDelay(i));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static BufferedImage prepare(BufferedImage orig) {
+        double scale = 0.1;
+        BufferedImage bi = new BufferedImage((int) (orig.getWidth() * scale), (int) (orig.getHeight() *scale),
+                BufferedImage.TYPE_BYTE_GRAY);
+        Graphics2D grph = (Graphics2D) bi.getGraphics();
+        grph.scale(scale, scale);
+        grph.drawImage(orig, 0, 0, null);
+        return bi;
+    }
+
+    private static String convertToASCII(BufferedImage image) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                int rgb = image.getRGB(x, y);
+                int intensity = (rgb & 0xff);
+                char c = PIXEL[intensity / STEP];
+                if (c == '#') {
+                    c = ' ';
+                }
+                stringBuilder.append(c);
+            }
+            stringBuilder.append("\n\r");
+        }
+        return stringBuilder.toString();
+    }
+
+    public static Frame[] getFrames() {
+        return frames;
+    }
+
+    public static class Frame {
+
+        private final String asciiImage;
+
+        private final int delayTime;
+
+        Frame(String asciiImage, int delayTime) {
+            this.asciiImage = asciiImage;
+            this.delayTime = delayTime;
+        }
+
+        public String getAsciiImage() {
+            return this.asciiImage;
+        }
+
+        public int getDelayTime() {
+            return this.delayTime;
+        }
+
+    }
+
+}
